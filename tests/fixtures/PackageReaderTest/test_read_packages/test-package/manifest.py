4f9bf6e91a78c7152c827db0fe9c72cd627b1abd
wndhydrnt <hydrantanderwand@gmail.com>
2021-12-02 21:58:52 +0100

    Fix a test

--- tests/fixtures/PackageReaderTest/test_read_packages/test-package/manifest.py
+++ tests/fixtures/PackageReaderTest/test_read_packages/test-package/manifest.py
@@ -2,4 +2,4 @@ from rcmt.package import Manifest
 from rcmt.package.action import Seed
 
 with Manifest(name="test-package") as manifest:
-    manifest.add_action(Seed("test.yaml", manifest.load_file("test.yaml")))
+    manifest.add_action(Seed(target="test.yaml", source="test.yaml"))
