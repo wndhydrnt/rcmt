df2e5c0eab7ba01e57c272802e5cb643a401ed79
Markus Meyer <hydrantanderwand@gmail.com>
2023-06-20 20:05:32 +0200

    Implement command "validate"

--- rcmt/__init__.py
+++ rcmt/__init__.py
@@ -2,9 +2,17 @@ import importlib.metadata
 
 from .rcmt import execute, options_from_config
 from .task import Run, Task
+from .validate import validate
 from .verify import execute as execute_verify
 
-__all__ = ["Run", "Task", "execute", "execute_verify", "options_from_config"]
+__all__ = [
+    "Run",
+    "Task",
+    "execute",
+    "execute_verify",
+    "options_from_config",
+    "validate",
+]
 
 try:
     __version__ = importlib.metadata.version(__name__)
