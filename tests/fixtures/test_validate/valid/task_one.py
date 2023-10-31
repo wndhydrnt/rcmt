0e93efd17c7740fdb34cf3637ff461d5c48ac23b
Markus Meyer <hydrantanderwand@gmail.com>
2023-10-31 11:32:05 +0100

    fix: validate command does not work with class-based Task definitions (#443)

---
+++ tests/fixtures/test_validate/valid/task_one.py
@@ -0,0 +1,18 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+from rcmt import Context, Task, register_task
+
+
+class TaskOne(Task):
+    name = "task-one"
+
+    def filter(self, ctx: Context) -> bool:
+        return False
+
+    def apply(self, ctx: Context) -> None:
+        return None
+
+
+register_task(TaskOne())
