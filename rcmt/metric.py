5427727372961beebaad8b5c2e5dd1fcc41a5608
Markus Meyer <hydrantanderwand@gmail.com>
2023-12-17 10:50:12 +0100

    define metrics

---
+++ rcmt/metric.py
@@ -0,0 +1,54 @@
+import random
+import string
+
+import structlog
+from prometheus_client import CollectorRegistry, Gauge, push_to_gateway
+
+from rcmt import config
+
+log: structlog.stdlib.BoundLogger = structlog.get_logger(package="metric")
+
+label_run = "".join(
+    random.choice(string.ascii_lowercase + string.digits) for _ in range(8)
+)
+registry = CollectorRegistry()
+
+run_start_timestamp = Gauge(
+    name="rcmt_run_start_timestamp",
+    documentation="Unix timestamp at which the latest run started.",
+    labelnames=["run_id"],
+    registry=registry,
+).labels(label_run)
+
+run_finish_timestamp = Gauge(
+    name="rcmt_run_finish_timestamp",
+    documentation="Unix timestamp at which the latest run finished.",
+    labelnames=["run_id"],
+    registry=registry,
+).labels(label_run)
+
+run_repositories_processed = Gauge(
+    name="rcmt_run_repositories_processed",
+    documentation="Repositories processed by the latest run of rcmt.",
+    labelnames=["run_id"],
+    registry=registry,
+).labels(label_run)
+
+run_error = Gauge(
+    name="rcmt_run_error",
+    documentation="Result of the latest run of rcmt. 0 indicates success, 1 indicates an error.",
+    labelnames=["run_id"],
+    registry=registry,
+).labels(label_run)
+
+
+def push(cfg: config.Pushgateway) -> None:
+    if cfg.enabled is False:
+        return None
+
+    log.debug("Sending metrics to pushgateway")
+    push_to_gateway(
+        gateway=cfg.address,
+        job=cfg.job_label,
+        registry=registry,
+    )
